0f2d9ea23236fcabb6c1bf4f7036e7dba10d5c47
sblaes <sebastianblaes@gmail.com>
2020-08-12 12:15:51 +0200

    modified js, added about page

--- js/main.js
+++ js/main.js
@@ -6,13 +6,24 @@ sitemap:
 
 $(document).ready(function () {
   $('a.panel-button').click(function (e) {
-    //if ($('.panel-cover').hasClass('panel-cover--collapsed')) return
+    new_hash = $(this).attr("href").substr(2)
+    $( ".content-element" ).each(function() {
+      if ($(this).hasClass(new_hash)){
+        $(this).css("display", "block")
+      }
+      else {
+        $(this).css("display", "none")
+      }
+    })
+    if ((window.location.hash) && !(new_hash == window.location.hash.substr(1))) return
+    // if ($('.panel-cover').hasClass('panel-cover--collapsed') && !(new_hash == window.location.hash.substr(1))) return
     if ($('.content-wrapper').hasClass('showing')){
       $('.content-wrapper').removeClass('animated slideInRight')
       $('.panel-cover').removeClass('panel-cover--collapsed')
       $('.panel-cover').css('max-width', '100%')
       $('.panel-cover').animate({'width': '100%'}, 400, swing = 'swing', function () {})
       $('.content-wrapper').removeClass('showing')
+      e.preventDefault();
       window.location.hash = '';
       parent.location.hash = ''
       return;
@@ -26,10 +37,19 @@ $(document).ready(function () {
       $('.panel-cover').animate({'max-width': '530px', 'width': '40%'}, 400, swing = 'swing', function () {})
     }
     $('.content-wrapper').addClass('showing');
+
   })
 
   if (window.location.hash && window.location.hash == '#projects') {
     $('.panel-cover').addClass('panel-cover--collapsed')
+    $('.projects').css("display", "block")
+    $('.about-me').css("display", "none")
+  }
+
+  if (window.location.hash && window.location.hash == '#about') {
+    $('.panel-cover').addClass('panel-cover--collapsed')
+    $('.projects').css("display", "none")
+    $('.about-me').css("display", "block")
   }
 
   if (window.location.pathname !== '{{ site.baseurl }}/' && window.location.pathname !== '{{ site.baseurl }}/index.html') {
