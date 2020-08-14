c1ddc1ab527cb519ff989197f63bce93d101977a
Sebastian Blaes <sebastianblaes@gmail.com>
2020-08-14 12:25:35 +0200

    added barbajs

--- js/main.js
+++ js/main.js
@@ -6,51 +6,35 @@ sitemap:
 
 $(document).ready(function () {
   $('a.panel-button').click(function (e) {
-    new_hash = $(this).attr("href").substr(2)
-    $( ".content-element" ).each(function() {
-      if ($(this).hasClass(new_hash)){
-        $(this).css("display", "block")
+    if ($(this)[0].pathname == '/') {
+      if ($('.content-wrapper').hasClass('showing')){
+        $('.content-wrapper').removeClass('animated slideInRight')
+        $('.panel-cover').removeClass('panel-cover--collapsed')
+        $('.panel-cover').css('max-width', '100%')
+        $('.panel-cover').animate({'width': '100%'}, 400, swing = 'swing', function () {})
+        $('.content-wrapper').removeClass('showing')
+        e.preventDefault();
+        window.location.hash = '';
+        parent.location.hash = ''
+        return;
       }
-      else {
-        $(this).css("display", "none")
+    }
+    if (window.location.pathname !== '{{ site.baseurl }}/' && window.location.pathname !== '{{ site.baseurl }}/index.html') {
+      return
+    }
+    else {
+      currentWidth = $('.panel-cover').width()
+      if (currentWidth < 960) {
+        $('.panel-cover').addClass('panel-cover--collapsed')
+        $('.content-wrapper').addClass('animated slideInRight')
+      } else {
+        $('.panel-cover').css('max-width', currentWidth)
+        $('.panel-cover').animate({'max-width': '530px', 'width': '40%'}, 400, swing = 'swing', function () {})
       }
-    })
-    if ((window.location.hash) && !(new_hash == window.location.hash.substr(1))) return
-    // if ($('.panel-cover').hasClass('panel-cover--collapsed') && !(new_hash == window.location.hash.substr(1))) return
-    if ($('.content-wrapper').hasClass('showing')){
-      $('.content-wrapper').removeClass('animated slideInRight')
-      $('.panel-cover').removeClass('panel-cover--collapsed')
-      $('.panel-cover').css('max-width', '100%')
-      $('.panel-cover').animate({'width': '100%'}, 400, swing = 'swing', function () {})
-      $('.content-wrapper').removeClass('showing')
-      e.preventDefault();
-      window.location.hash = '';
-      parent.location.hash = ''
-      return;
+      $('.content-wrapper').addClass('showing');
     }
-    currentWidth = $('.panel-cover').width()
-    if (currentWidth < 960) {
-      $('.panel-cover').addClass('panel-cover--collapsed')
-      $('.content-wrapper').addClass('animated slideInRight')
-    } else {
-      $('.panel-cover').css('max-width', currentWidth)
-      $('.panel-cover').animate({'max-width': '530px', 'width': '40%'}, 400, swing = 'swing', function () {})
-    }
-    $('.content-wrapper').addClass('showing');
 
   })
-
-  if (window.location.hash && window.location.hash == '#projects') {
-    $('.panel-cover').addClass('panel-cover--collapsed')
-    $('.projects').css("display", "block")
-    $('.about-me').css("display", "none")
-  }
-
-  if (window.location.hash && window.location.hash == '#about') {
-    $('.panel-cover').addClass('panel-cover--collapsed')
-    $('.projects').css("display", "none")
-    $('.about-me').css("display", "block")
-  }
 
   if (window.location.pathname !== '{{ site.baseurl }}/' && window.location.pathname !== '{{ site.baseurl }}/index.html') {
     $('.panel-cover').addClass('panel-cover--collapsed')
